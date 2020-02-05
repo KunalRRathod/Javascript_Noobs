d96151e1ce5e5df363bb9ec90fb0f2f7fcee4694
Kunal Rathod <36470138+KunalRRathod@users.noreply.github.com>
2020-02-05 03:00:39 -0800

    JS code in HTML files

---
+++ intojs.js
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <button onclick="myFunction()">Click</button>
+<script>
+    function myFunction() {
+        varage= 20;
+        if (age >= 18){
+            alert("You are entitled to vote")
+        }
+        else {
+            alert("Grow UP first !")
+        }
+        
+    }
+</script>
+</body>
+</html>
